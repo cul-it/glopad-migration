a4968f6114497a34a8d9ac005e59d9d8d80ab20c
James Reidy <jreidy@cornell.edu>
2017-01-24 14:36:05 -0500

    scripts to update server GloPAD code from git

---
+++ scripts/glopad-rsync-github/upload-glopad.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+REMOTE_USER="jgr25"
+REMOTE_MACHINE="sf-lib-dig-005.serverfarm.cornell.edu"
+REMOTE_PATH="/arts-lib/sites/www.glopad.org"
+LOCAL_PATH="/Users/jgr25/Documents/www-glopad-org"
+
+
+rsync -avcz -e "ssh -l $REMOTE_USER" --omit-dir-times --no-perms --no-times \
+  --exclude=cache \
+  --exclude=templates_c \
+  --exclude=var/tmp \
+  --exclude=drupal_files \
+  --exclude=media \
+  --exclude=old_cache \
+  --exclude=htdocs/phpPgAdmin \
+  --exclude=.svn --exclude=.git  --exclude=.gitignore \
+  --exclude=.DS_Store \
+  --exclude=htdocs/jparc/files \
+  $LOCAL_PATH/ $REMOTE_USER@$REMOTE_MACHINE:$REMOTE_PATH/ || error_exit "can't move site files"
